893bad282d65ec5847d852de1066f0ee5d9820e4
Fran Sosa <franciscososa91@gmail.com>
2021-11-07 18:44:10 -0300

    Subida de Ejercicios Resueltos

--- ejercicios_practica/ejercicio1_.py
+++ ejercicios_practica/ejercicio1_.py
@@ -8,7 +8,9 @@
 # que aparecen en verde con el hashtag "#"
 
 import json
+from json.decoder import JSONDecodeError
 
+file = 'mi_json.json'
 
 def serializar():
     print("Funcion que genera un archivo JSON")
@@ -26,12 +28,27 @@ def serializar():
     # Que su lista de prendas dentro del JSON tenga al menos 2 prendas
 
     # json_data = {...}
-
+    json_data = {
+        "nombre": "Fracisco",
+        "apellido": "Sosa",
+        "Vestimenta":[
+        { "prenda": "zapatilla", "cantidad": 2 },
+        { "prenda": "remeras", "cantidad": 4 },
+        { "prenda": "camisas", "cantidad": 4 },
+        { "prenda": "jeans", "cantidad": 5 },
+        ]
+    }
     # Una vez que finalice el JSON realice un "dump" para almacenarlo en
     # un archivo que usted defina
-
+    print('Imprimir json como un objeto')
+    print(json_data)
     # Observe el archivo y verifique que se almaceno lo deseado
-
+    with open(file, 'w') as jsonfile:
+        data = [json_data]
+        json.dump(data, jsonfile, indent=4)
+    print('Imprimo el archivo')
+    print(json.dumps(data, indent=4))
+    print('Funcion serialize finalizó')
 
 def deserializar():
     print("Funcion que lee un archivo JSON")
@@ -40,14 +57,22 @@ def deserializar():
     # del archivo y guardarlo en un objeto JSON utilizando el método
     # load()
 
+    nvo_json = {}
+    with open(file, 'r') as jsonfile:
+        nvo_json = json.load(jsonfile)
+    
     # Luego debe convertir ese JSON data en json_string utilizando
     # el método "dumps" y finalmente imprimir en pantalla el resultado
+    with open('mi_json.json', 'w') as jsonfile:
+        json.dump(nvo_json,jsonfile, indent=4)
     # Recuerde utilizar indent=4 para poder observar mejor el resultado
     # en pantalla y comparelo contra el JSON que generó en la función anterior
+    print('Imprimo el archivo deserializado')
+    print(json.dumps(nvo_json, indent=4))
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-    
+
     serializar()
     deserializar()
 
